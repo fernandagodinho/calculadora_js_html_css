070d5374abb6e17051adb4cc986aca554a3a0804
fernandagodinho <fgodinhosilva@gmail.com>
2024-07-15 12:46:00 -0300

    a

---
+++ calculadora02.js
@@ -0,0 +1,53 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function clearDisplay() {
+    currentInput = '';
+    operator = '';
+    previousInput = '';
+    display.innerText = '0';
+}
+
+function appendNumber(number) {
+    currentInput += number;
+    display.innerText = currentInput;
+}
+
+function appendOperator(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentInput = result;
+    operator = '';
+    previousInput = '';
+    display.innerText = result;
+}
